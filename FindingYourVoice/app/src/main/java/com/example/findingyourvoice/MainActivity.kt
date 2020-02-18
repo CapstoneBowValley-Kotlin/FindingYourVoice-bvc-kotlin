5902bcd086dfdc089db1d9e51ab623038bb5559a
Preston Fabbi <preston_fbs@hotmail.com>
2020-02-18 13:45:26 -0700

    Created homepage layout

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/MainActivity.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.findingyourvoice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myIntent = Intent(this, Resources::class.java)
+        // Reference home_button
+        // setOnClickListener: A user interface element the user can tap or click to perform an action
+        // Log.d: Send a DEBUG log message and log the exception.
+        resource_button.setOnClickListener {
+            Log.d("RESOURCES", "Button is clicked")
+
+            startActivity(myIntent)
+        }
     }
 }
