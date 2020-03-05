40fcbc872e0af2c7dcd1c9605d2946f530cc6571
Hasrat Singh <hanshasrat@gmail.com>
2020-03-05 13:27:51 -0700

    Created CaseChat1 Activity

---
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/Chat_Case1.kt
@@ -0,0 +1,12 @@
+package com.example.findingyourvoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chat_Case1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat__case1)
+    }
+}
