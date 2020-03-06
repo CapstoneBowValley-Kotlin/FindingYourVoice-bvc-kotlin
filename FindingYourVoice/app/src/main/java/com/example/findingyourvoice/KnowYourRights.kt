532775c31964c4e216ffd0f0f31f5b147a439f5a
Issacthaper <Issacthaper27@gmail.com>
2020-03-06 09:50:42 -0700

    Designed the fragments layout which contains OHS, Employment standards and Human Rights. App is crashing after that.

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/KnowYourRights.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/KnowYourRights.kt
@@ -5,8 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.findingyourvoice.MainActivity
-import com.example.findingyourvoice.R
+import com.example.findingyourvoice.*
 import kotlinx.android.synthetic.main.activity_know_your_rights.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,5 +19,41 @@ class KnowYourRights : AppCompatActivity() {
             Log.d("Know Your Rights", "I am in Know Your Rights Activity")
             startActivity(intent)
         }
+
+
+        loadOHS(OHSFragment())
+        OHS_button.setOnClickListener {
+            loadOHS(OHSFragment())
+        }
+
+        Human_Rights_button.setOnClickListener {
+            loadHuman_Rights(Human_RightsFragment())
+        }
+
+        empStandard_button.setOnClickListener {
+            loadEmployment_Standards(Employment_StandardsFragment())
+        }
+
+
+    }
+
+    private fun loadOHS(ohsFragment: OHSFragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_oh, ohsFragment)
+        ft.commit()
+
+    }
+
+    private fun loadHuman_Rights(humanrightsFragment: Human_RightsFragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.Human_RightsFragment, humanrightsFragment)
+        ft.commit()
+
+    }
+
+    private fun loadEmployment_Standards(employmentStandardsfragment: Employment_StandardsFragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.Employment_StandardsFragment, employmentStandardsfragment)
+        ft.commit()
     }
 }
