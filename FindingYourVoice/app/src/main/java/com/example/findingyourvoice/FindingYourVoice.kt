b6fbec53ae00cd4ee86c178d9c63426f1646d695
Hasrat Singh <hanshasrat@gmail.com>
2020-03-06 00:25:00 -0700

    Removed Conflicts.

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/FindingYourVoice.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/FindingYourVoice.kt
@@ -12,12 +12,13 @@ class FindingYourVoice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finding_your_voice)
-        val intent = Intent(this,MainActivity::class.java)
+        val intentMenu = Intent(this,Menu::class.java)
         Home_imageView.setOnClickListener {
-            startActivity(intent)
+            startActivity(intentMenu)
         }
+        val intentChatCase1 = Intent(this,Chat_Case1::class.java)
         Next_button.setOnClickListener {
-
+        startActivity(intentChatCase1)
         }
     }
 }
