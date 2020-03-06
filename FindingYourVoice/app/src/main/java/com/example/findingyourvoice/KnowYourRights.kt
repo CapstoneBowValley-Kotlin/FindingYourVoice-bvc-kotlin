de4e2df0b0ec76c73f8eedc4b6816c8326d6bd97
UrvashiThapar <u.thapar753@mybvc.ca>
2020-03-05 21:41:20 -0700

    Modified Workplace safety main page. Added 3 fragments.

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/KnowYourRights.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/KnowYourRights.kt
@@ -15,7 +15,7 @@ class KnowYourRights : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_know_your_rights)
-        KYRHome_button.setOnClickListener {
+        Workplace_safety_home_imageicon.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             Log.d("Know Your Rights", "I am in Know Your Rights Activity")
             startActivity(intent)
