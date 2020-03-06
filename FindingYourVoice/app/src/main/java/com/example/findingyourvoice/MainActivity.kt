c188fb588401a268b04bc44ee6c700e303c5fd76
Seerat Kaur <seeratchahal8@gmail.com>
2020-03-05 23:50:54 -0700

    Seerat and Gagan-Work done on Menu

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/MainActivity.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myIntent = Intent(this, Resources::class.java)
+       val myIntent = Intent(this, Resources::class.java)
 
         // Reference home_button
         // setOnClickListener: A user interface element the user can tap or click to perform an action
@@ -22,6 +22,17 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(myIntent)
         }
+
+       /* val myIntent = Intent(this, Menu::class.java)
+
+        // Reference home_button
+        // setOnClickListener: A user interface element the user can tap or click to perform an action
+        // Log.d: Send a DEBUG log message and log the exception.
+        resource_button.setOnClickListener {
+            Log.d("MENU", "Button is clicked")
+
+            startActivity(myIntent)
+        }*/
         FYV_button.setOnClickListener {
       val intent =Intent(this,FindingYourVoice::class.java)
           startActivity(intent)
