e0606d36f853df504c574aab831bf638df38a725
Sukhdeep Kaur <sukhdeepdeol92@gmail.com>
2020-03-09 12:57:47 -0600

    Designed Navigating the system

---
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/NavigatingTheSystem.kt
@@ -0,0 +1,12 @@
+package com.example.findingyourvoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavigatingTheSystem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigating_the_system)
+    }
+}
