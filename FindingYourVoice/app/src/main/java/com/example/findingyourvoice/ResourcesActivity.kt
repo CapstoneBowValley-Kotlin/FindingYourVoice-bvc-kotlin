2ad97b60243bc795a89f579ba9ee5ce33457aaac
Hasrat Singh <hanshasrat@gmail.com>
2020-03-11 22:56:47 -0600

    Added functionality to home button in Resources page

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/ResourcesActivity.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/ResourcesActivity.kt
@@ -6,12 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_resources2.*
+import kotlinx.android.synthetic.main.app_bar.*
 
 class ResourcesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resources2)
+
+        imageView2.setOnClickListener {
+            val intent =Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
 
         btnCall1.setOnClickListener {
             val dialog = AlertDialog.Builder(this)
