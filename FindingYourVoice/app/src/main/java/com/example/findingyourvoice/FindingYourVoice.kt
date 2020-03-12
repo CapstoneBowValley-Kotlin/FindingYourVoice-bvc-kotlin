db234a8a3b08be2f1bb697c3af6ef5d2c1d973c8
Gagandeep Bhullar <gaganbhullar5911@gmail.com>
2020-03-12 11:20:02 -0600

    menu work done by gagan and seerat

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/FindingYourVoice.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/FindingYourVoice.kt
@@ -12,19 +12,19 @@ import kotlinx.android.synthetic.main.activity_resources.KYRHome_button
 import androidx.fragment.app.Fragment
 
 class FindingYourVoice : AppCompatActivity() {
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finding_your_voice)
-      /*  val intentMenu = Intent(this,Menu::class.java)
-        Home_imageView.setOnClickListener {
-            startActivity(intentMenu)
-        }*/
+        /*  val intentMenu = Intent(this,Menu::class.java)
+          Home_imageView.setOnClickListener {
+              startActivity(intentMenu)
+          }*/
         val intentChatCase1 = Intent(this,Chat_Case1::class.java)
         Next_button.setOnClickListener {
-        startActivity(intentChatCase1)
+            startActivity(intentChatCase1)
         }
     }
+
+
+
 }
--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/ResourcesActivity.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/ResourcesActivity.kt
@@ -2,11 +2,16 @@ package com.example.findingyourvoice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.app_bar.*
 
 class ResourcesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resources2)
+
+        btnMenu.setOnClickListener {
+
+        }
     }
 }
