efc6b6589f0c62754121074baa319a0b5f0175fd
UrvashiThapar <u.thapar753@mybvc.ca>
2020-04-14 17:36:33 -0600

    make links functional in Emp Standard fragment.

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/Employment_StandardsFragment.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/Employment_StandardsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.findingyourvoice
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -36,6 +37,17 @@ class Employment_StandardsFragment : Fragment() {
         }
         FYV_EMP_STD_button.setOnClickListener {
             (activity as? ParentActivity)?.openFindVoice()
+        }
+
+        Emp_std_code_link_button.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://www.qp.alberta.ca/documents/Acts/E09.pdf")))
+        }
+        emp_std_regulations_link_button.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://www.qp.alberta.ca/documents/Regs/1997_014.pdf")))
+        }
+
+        Emp_std_rules_link_button.setOnClickListener{
+            startActivity((Intent(Intent.ACTION_VIEW, Uri.parse("https://www.alberta.ca/alberta-employment-standards-rules.aspx"))))
         }
     }
 }
