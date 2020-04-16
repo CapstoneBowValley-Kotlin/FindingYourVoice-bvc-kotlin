2f1ac28bab8e1c567c0b6a61ca80fd6186cec628
Sukhdeep Kaur <s.kaur273@mybvc.ca>
2020-04-16 15:30:41 -0600

    Added comments

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/NavigatingTheSystem.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/NavigatingTheSystem.kt
@@ -11,10 +11,15 @@ class NavigatingTheSystem : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigating_the_system)
+
+        //On clicking the particular card view ,intent takes the user to Chat_Case1 activity
+
         val intentChatCase1 = Intent(this,Chat_Case1::class.java)
         cardView_first.setOnClickListener {
             startActivity(intentChatCase1)
         }
+
+        //on clicking the home button at top right corner , intent takes the user to MainActivity
         val main = Intent(this,MainActivity::class.java)
         btnHome.setOnClickListener {
             startActivity(main)
