0875cdeafa4f1d0d74fd2f3418325b0a94c46d23
Gagandeep Bhullar <g.bhullar854@mybvc.ca>
2020-04-17 03:43:46 -0600

    work on fragments under OHS

--- FindingYourVoice/app/src/main/java/com/example/findingyourvoice/fragments/OHSPlaceholderFragment.kt
+++ FindingYourVoice/app/src/main/java/com/example/findingyourvoice/fragments/OHSPlaceholderFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.findingyourvoice.R
+import com.example.findingyourvoice.SliderModel
+import com.example.findingyourvoice.adapters.SliderAdapter
+import kotlinx.android.synthetic.main.fragment_ohs_place_holder.*
 
 
 class OHSPlaceholderFragment : Fragment() {
@@ -17,6 +20,44 @@ class OHSPlaceholderFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_ohs_place_holder, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = SliderAdapter(getList())
+
+        slider.adapter = adapter
+
+        btnNext.setOnClickListener {
+            val nextIndex = if (slider.currentItem < getList().size - 1) {
+                slider.currentItem + 1
+            }
+            else {
+                0
+            }
+
+            slider.setCurrentItem(nextIndex, true)
+
+        }
+        btnPrev.setOnClickListener {
+            val prevIndex = if (slider.currentItem > 0) {
+                slider.currentItem - 1
+            }
+            else {
+                getList().size - 1
+            }
+            slider.setCurrentItem(prevIndex, true)
+        }
+    }
+
+    private fun getList(): ArrayList<SliderModel> {
+        val list = ArrayList<SliderModel>()
+        list.add(SliderModel("KNOW", "about the dangers of our jobs and how we are protected"))
+        list.add(SliderModel("PARTICIPATE", "in activities affecting our Health and Safety"))
+        list.add(SliderModel("REFUSE WORK", "we feel may be dangerous to ourselves or others"))
+        list.add(SliderModel("BE FREE FROM \n REPRISAL", "(disciplined or fired) for using our Health and Safety rights"))
+        return list
     }
 
     companion object {
